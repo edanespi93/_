76913015de311b242664f9292bea39391380554b
edanespi93 <73562015+edanespi93@users.noreply.github.com>
2021-02-17 00:25:15 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 import { animate } from "./animations.js";
 import { ContactForm } from "./contacto.js";
+import responsiveSlider from "./slider.js";
 
-const d=document,
-$btnMenu=d.querySelector(".menu-btn"),
-$menu=d.querySelector(".menu");
-ScrollReveal().reveal('home');
-ScrollReveal().reveal('.me', {delay: 500});
-ScrollReveal().reveal('.proyectos', {delay: 500});
-ScrollReveal().reveal('.contacto', {delay: 500});
+const d = document,
+  $btnMenu = d.querySelector(".menu-btn"),
+  $menu = d.querySelector(".menu");
+ScrollReveal().reveal("home");
+ScrollReveal().reveal(".me", { delay: 500 });
+ScrollReveal().reveal(".proyectos", { delay: 500 });
+ScrollReveal().reveal(".contacto", { delay: 500 });
 
-d.addEventListener("DOMContentLoaded",e=>{
+d.addEventListener("DOMContentLoaded", (e) => {
   animate();
   ContactForm();
-$btnMenu.addEventListener("click",e=>{
-  $btnMenu.firstElementChild.classList.toggle("none");
-  $btnMenu.lastElementChild.classList.toggle("none");
-  $menu.classList.toggle("is-active")
-})
-d.addEventListener("click",e=>{
-  if(!e.target.matches(".menu a"))return false;
- $btnMenu.firstElementChild.classList.remove("none");
-  $btnMenu.lastElementChild.classList.add("none");
-  $menu.classList.remove("is-active")
-
-})
-
-})
+  responsiveSlider();
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu a")) return false;
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+});
